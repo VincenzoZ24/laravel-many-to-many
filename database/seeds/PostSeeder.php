6283f441f61d4ddb78eedecbdbdfdbf959f9a962
VincenzoZ24 <zitov287@gmail.com>
2023-02-03 09:57:23 +0100

    add modifiche

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -24,21 +24,21 @@ class PostSeeder extends Seeder
         for ($i = 0; $i < 1000; $i++) {
             $title = $faker->words(rand(3, 7), true);
 
-            $number = rand(0, 276);
-            if ($number) {
-                $contents = new File(__DIR__ . '/../../storage/app/lorempicsum/picsum' . $number . '.jpg');
+            // $number = rand(0, 276);
+            // if ($number) {
+            //     $contents = new File(__DIR__ . '/../../storage/app/lorempicsum/picsum' . $number . '.jpg');
                
-                $img_path = Storage::put('uploads', $contents);
-            } else {
-                $img_path = null;
-            }
+            //     $img_path = Storage::put('uploads', $contents);
+            // } else {
+            //     $img_path = null;
+            // }
 
             $post = Post::create([
                 'category_id'   => $faker->randomElement($categories)->id,
                 'slug'          => Post::getSlug($title),
                 'title'         => $title,
-                'image'         => 'https://picsum.photos/id/'. rand(0, 1000) .'/500/400',
-                'uploaded_img'  => $img_path,
+                // 'image'         => 'https://picsum.photos/id/'. rand(0, 1000) .'/500/400',
+                // 'uploaded_img'  => $img_path,
                 'content'       => $faker->paragraphs(rand(1, 10), true),
                 'excerpt'       => $faker->paragraph(),
             ]);
